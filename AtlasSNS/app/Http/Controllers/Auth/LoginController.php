18404d66db169dccd90a95871f6d8382f1b4630b
のすけ <>
2022-06-16 00:01:26 +0900

    authミドルウェアのリダイレクト先の指定(ログイン中ユーザー用)。ミドルウェア補足追記。

--- AtlasSNS/app/Http/Controllers/Auth/LoginController.php
+++ AtlasSNS/app/Http/Controllers/Auth/LoginController.php
@@ -38,6 +38,7 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        // ログイン済みユーザーはログイン及び登録画面に移動しようとしたとき、/topに移動する。
     }
 
     public function login(Request $request){
--- AtlasSNS/app/Http/Controllers/PostsController.php
+++ AtlasSNS/app/Http/Controllers/PostsController.php
@@ -10,6 +10,7 @@ class PostsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        // ログインしていないユーザーがログイン後のページに移動しようとしたとき/loginに移動させる。
     }
 
     public function index(){
