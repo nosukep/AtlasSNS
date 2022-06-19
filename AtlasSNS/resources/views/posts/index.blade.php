777a76d5b18418ae49442c5b0e4ff9d6a825eec3
のすけ <>
2022-06-19 11:55:51 +0900

    投稿フォーム設置

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。</h2>
+    <div class="container">
+        {!! Form::open(['url' => 'top']) !!}
+        <div class="form-group">
+            {!! Form::input('text', 'newPost', null, ['required', 'class' => 'post-form', 'placeholder' => '投稿内容を入力してください']) !!}
+        </div>
+        <button type="submit" class="btn btn-success pull-right"></button>
+        {!! Form::close() !!}
+
+        <table>
+        @foreach ($lists as $list)
+          <tr>
+              <td>{{ $list->id }}</td>
+              <td>{{ $list->post }}</td>
+              <td>{{ $list->created_at }}</td>
+              <td><a class="btn btn-primary" href="/post/{{$list->id}}/update-form">更新</a></td>
+              <td><a class="btn btn-danger" href="/post/{{$list->id}}/delete" onclick="return confirm('こちらの投稿を削除してもよろしいでしょうか？')">削除</a></td>
+          </tr>
+        @endforeach
+        </table>
+    </div>
+
 
 @endsection
