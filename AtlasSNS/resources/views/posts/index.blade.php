fd1410e63babf6385cfdce704c95cb23c2d6fde5
のすけ <>
2022-06-22 21:50:51 +0900

    投稿フォーム、削除ボタン、編集ボタンのCSS調整

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -19,12 +19,12 @@
         <table>
         @foreach ($lists as $list)
           <tr>
-              <td><img src={{ $list->images }} alt="プロフィール画像"></td>
+              <td class="user-icon"><img src={{ $list->images }} alt="プロフィール画像"></td>
               <td>{{ $list->username }}</td>
               <td>{{ $list->post }}</td>
               <td>{{ $list->created_at }}</td>
-              <td><a class="btn btn-primary" href="/post/{{$list->id}}/update-form">更新</a></td>
-              <td><a class="btn btn-danger" href="/post/{{$list->id}}/delete" onclick="return confirm('こちらの投稿を削除してもよろしいでしょうか？')">削除</a></td>
+              <td><a class="btn btn-primary" href="/post/{{$list->id}}/update-form"><img src="/storage/images/edit.png" alt="編集"></a></td>
+              <td><a class="btn btn-danger" href="/post/{{$list->id}}/delete" onclick="return confirm('こちらの投稿を削除してもよろしいでしょうか？')"><img src="/storage/images/trash.png" alt="削除"></a></td>
           </tr>
         @endforeach
         </table>
