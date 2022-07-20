9b76566f78ace64d8f8e55a9fcea0fded9ad90dc
のすけ <>
2022-07-21 06:33:57 +0900

    登録完了ページのCSS調整

--- AtlasSNS/resources/views/auth/added.blade.php
+++ AtlasSNS/resources/views/auth/added.blade.php
@@ -3,12 +3,10 @@
 @section('content')
 
 <div id="clear">
-  <p>{{ Session::get('username') }}さん</p>
-  <p>ようこそ！AtlasSNSへ！</p>
-  <p>ユーザー登録が完了しました。</p>
-  <p>早速ログインをしてみましょう。</p>
+  <p class="login-title">{{ Session::get('username') }}さん<br>ようこそ！AtlasSNSへ！</p>
+  <p>ユーザー登録が完了しました。<br>早速ログインをしてみましょう。</p>
 
-  <p class="btn"><a href="/login">ログイン画面へ</a></p>
+  <p class="btn btn-danger"><a href="/login">ログイン画面へ</a></p>
 </div>
 
 @endsection
